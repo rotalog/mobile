85c2b65114ed87ad17e516f50a79a1df267d1b82
Ivan Matheus <ivanmatheus29@gmail.com>
2026-03-21 13:13:42 -0400

    Feature/mob 5 criacao projeto (#1)
    * commit fofinho
    
    * feat(starting): inicializando o projeto [MOB-5]
    
    * feat(starting): inicializando o projeto [MOB-5]
    
    * feat(starting): inicializando o projeto [MOB-5]

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        '@navigation': './src/navigation',
+        '@contexts': './src/contexts',
+        '@shared': './src/shared',
+        '@buyer': './src/buyer',
+        '@driver': './src/driver',
+        '@hooks': './src/hooks',
+      }
+    }]
+  ]
 };
